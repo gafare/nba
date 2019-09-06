5738131c0f931e1464884a288766fd77418504f8
gafare <43350807+gafare@users.noreply.github.com>
2019-09-06 14:03:27 -0400

    Add files via upload

---
+++ NBA roster/script.js
@@ -0,0 +1,15 @@
+Const items = [
+    {
+        "name":"t-shirt",
+        "price":12.99,
+        "supply":4
+        "shippingCost":2.99
+        "img":"tshirt.png"
+    }
+    {
+    "name":"coffee mug",
+    "price":4.99,
+     "supply":1,
+    }
+]
+    
